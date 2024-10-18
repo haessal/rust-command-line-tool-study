417954c37a01ccfa58437a3e7091b0a338d37d7c
haessal <haessal@mizutamauki.net>
2024-10-18 15:53:25 +0900

    try to use clap builder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::{Arg, ArgAction, Command};
+
 fn main() {
+    let matches = Command::new("rCLI")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("haessal, haessal@mizutamauki.net")
+        .about("My command line inferface tools")
+        .arg(
+            Arg::new("debug")
+                .short('d')
+                .long("debug")
+                .action(ArgAction::SetTrue)
+                .help("Enable debug mode"),
+        )
+        .get_matches();
+    if matches.get_flag("debug") {
+        println!("Debug mode enabled");
+    }
+
     println!("Hello, world!");
 }
